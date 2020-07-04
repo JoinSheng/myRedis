283901961ab7dc5bd0223a13c164fc69103f547d
JoinSheng <zscaoc@isoftstone.com>
2020-07-04 23:41:26 +0800

    第一次提交

---
+++ src/com/itcast/jedis/web/ProvinceServlet.java
@@ -0,0 +1,40 @@
+package com.itcast.jedis.web;
+
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.itcast.jedis.domain.Province;
+import com.itcast.jedis.service.ProvinceService;
+import com.itcast.jedis.service.impl.ProvinceServiceImpl;
+
+import javax.jws.WebService;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.util.List;
+
+/**
+ * @Author: JoinCao
+ * @CreateDate: 2020-06-06
+ */
+@WebServlet("province")
+public class ProvinceServlet extends HttpServlet {
+    @Override
+    protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        ProvinceService provinceService = new ProvinceServiceImpl();
+        List<Province> list = provinceService.findAll();
+        //序列化list为json
+        ObjectMapper mapper = new ObjectMapper();
+        String json = mapper.writeValueAsString(list);
+
+        resp.setContentType("application/json;charset=utf-8");
+        resp.getWriter().write(json);
+    }
+
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        this.doPost(req, resp);
+    }
+}
